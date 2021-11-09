6f5eea7979f15274c9bb8701604bb968e1e85613
Lukas Nittka <lukas.nittka@gmail.com>
2021-11-09 12:57:46 +0100

    copied theme from github repo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
 import { createTheme , ThemeProvider } from '@material-ui/core/styles';
+import './App.css';
+import CustomBttn from './components/CustomBttn';
+
 
 const theme = createTheme({
-  
-})
+  palette: {
+    primary: {
+      main:"#42a5f5",
+    },
+    secondary: {
+      main:"#c7d8ed",
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Roboto'
+    ],
+    h4: {
+      fontWeight: 600,
+      fontSize: 28,
+      lineHeight: '2rem',
+      },
+    h5: {
+      fontWeight: 100,
+      lineHeight: '2rem',
+    },
+  },
+});
 
 function App() {
   return (
     <div className="App">
       <ThemeProvider theme = {theme}>
+
       </ThemeProvider>
-      <header className="App-header">
-        Hello There
-      </header>
     </div>
   );
 }
