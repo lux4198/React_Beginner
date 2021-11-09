6988c3f440beca2e38eaaf5ef895e6d783b297de
Lukas Nittka <lukas.nittka@gmail.com>
2021-11-09 11:26:12 +0100

    added MUI to app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { createTheme , ThemeProvider } from '@material-ui/core/styles';
+
+const theme = createTheme({
+  
+})
 
 function App() {
   return (
     <div className="App">
+      <ThemeProvider theme = {theme}>
+      </ThemeProvider>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Hello There
       </header>
     </div>
   );
